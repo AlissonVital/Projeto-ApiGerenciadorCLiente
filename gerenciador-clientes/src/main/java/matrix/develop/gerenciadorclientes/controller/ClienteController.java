5531ed938a909e05583a1d52e5ec4fe3202ffdc5
Alisson Vital <alisson.costa-terceiro@cea.com.br>
2023-12-02 10:07:25 -0300

    Metodo Get parte 02, pesquisa por id, algumas configurações nos status do sistema

--- gerenciador-clientes/src/main/java/matrix/develop/gerenciadorclientes/controller/ClienteController.java
+++ gerenciador-clientes/src/main/java/matrix/develop/gerenciadorclientes/controller/ClienteController.java
@@ -5,11 +5,15 @@ import lombok.extern.log4j.Log4j2;
 import matrix.develop.gerenciadorclientes.domain.Cliente;
 import matrix.develop.gerenciadorclientes.service.ClienteService;
 import matrix.develop.gerenciadorclientes.util.DateUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 //      -----Gerencia os EndPoints-----
@@ -24,10 +28,15 @@ public class ClienteController {
 
     private final ClienteService clienteService;
 
-
+    //transformar os dados em uma String para ser acessados por requisição
     @GetMapping(path = "list/cliente")
-    public List<Cliente> list() {
+    public ResponseEntity<List<Cliente>> list() {
         log.info(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
-        return clienteService.listAll();
+        return ResponseEntity.ok(clienteService.listAll());
+    }
+
+    @GetMapping(path = "list/cliente/{id}")
+    public ResponseEntity<Cliente> findById(@PathVariable long id) {
+        return ResponseEntity.ok(clienteService.findById(id));
     }
 }
--- gerenciador-clientes/src/main/java/matrix/develop/gerenciadorclientes/service/ClienteService.java
+++ gerenciador-clientes/src/main/java/matrix/develop/gerenciadorclientes/service/ClienteService.java
@@ -1,7 +1,9 @@
 package matrix.develop.gerenciadorclientes.service;
 
 import matrix.develop.gerenciadorclientes.domain.Cliente;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -11,25 +13,28 @@ import java.util.List;
 
 @Service
 public class ClienteService {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    LocalDate dataCadastro = LocalDate.parse("02/12/2023", formatter);
+    private List<Cliente> cliente = List.of(
+            new Cliente(1L, "Alisson Ferreira", "33508590811", null, "409613666", null, "2974503", "20524503", "981199630", dataCadastro, "ATIVO"),
+            new Cliente(2L, "Marcia Porto", "23422344144", null, "123456789", null, "24253748", "53040320", "953040320", dataCadastro, "ATIVO"),
+            new Cliente(3L, "Egina Vital", "23423553945", null, "765432789", null, "20304050", "59090900", "959090900", dataCadastro, "ATIVO"),
+            new Cliente(4L, "Silson Ferreira", "54676554654", null, "132446869", null, "20292873", "58839280", "958839280", dataCadastro, "ATIVO"),
+            new Cliente(5L, "Zelita Porto", "53453464576", null, "315362786", null, "28039484", "58893808", "958893808", dataCadastro, "ATIVO"),
+            new Cliente(6L, "Aparecidi Nougueira", "46546345345", null, "412641857", null, "25093459", "57838379", "957838379", dataCadastro, "ATIVO"),
+            new Cliente(7L, "Maria Nougueira", "7626345345", null, "46341857", null, "20433459", "578034979", "954118379", dataCadastro, "ATIVO"),
+            new Cliente(6L, "Nalva Porto", "4622455345", null, "412641457", null, "26433459", "57823379", "957823579", dataCadastro, "INATIVO")
+    );
 
+    public List<Cliente> listAll() {
+        return cliente;
+    }
 
-    public List<Cliente> listAll(){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate dataCadastro = LocalDate.parse("01/12/2023", formatter);
-
-        return List.of(
-                new Cliente(1L, "Alisson Ferreira", "33508590811", null, "409613666", null, "2974503", "20524503", "981199630", dataCadastro, "ATIVO"),
-
-                new Cliente(2L, "Marcia Porto", "23422344144", null, "123456789", null, "24253748", "53040320", "953040320", dataCadastro, "ATIVO"),
-
-                new Cliente(3L, "Egina Vital", "23423553945", null, "765432789", null, "20304050", "59090900", "959090900", dataCadastro, "ATIVO"),
-
-                new Cliente(4L, "Silson Ferreira", "54676554654", null, "132446869", null, "20292873", "58839280", "958839280", dataCadastro, "ATIVO"),
-
-                new Cliente(5L, "Zelita Porto", "53453464576", null, "315362786", null, "28039484", "58893808", "958893808", dataCadastro, "ATIVO"),
-
-                new Cliente(6L, "Aparecidi Nougueira", "46546345345", null, "412641857", null, "25093459", "57838379", "957838379", dataCadastro, "ATIVO")
-        );
+    public Cliente findById(long id) {
+        return cliente.stream()
+                .filter(cliente -> cliente.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Celiente not Found"));
     }
 }
 
